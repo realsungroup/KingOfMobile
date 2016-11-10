50a5005f6cf7a4cf4300aa9cc3f224fd9d7af37f
fujia <5211666@qq.com>
2016-11-10 20:39:52 +0800

    test

--- source/app/mywork/viewmodels/mywork2.js
+++ source/app/mywork/viewmodels/mywork2.js
@@ -1,24 +1,30 @@
 define(['durandal/app','knockout','plugins/router','plugins/dialog','mywork/mywork'], function (app,ko,router,dialog,mywork) {
    
-  
+      
        return  {
                 activate:function () {
-                         
-                          mywork.subtitle("mywork2");
-                          var navCls=$("#example-navbar-collapse").attr("class");
+                       
+                       
+                        var myrouter=mywork.getCurroute(this);
+                        console.log(myrouter);
+
+                        var navCls=$("#example-navbar-collapse").attr("class");
                           if (navCls=="navbar-collapse collapse in")
                                 {
                                         $(".navbar-toggle").trigger("click");
                                 }
-
+                         
                 },
-                 attached:function () {
-                       // console.log(mywork);
-                        mywork.subtitle("mywork2");
+                attached:function () {
+                      
                        
                        
 
                 },
+                compositionComplete:function(){
+                       
+                }
+
               
         };
 }); 
--- source/app/mywork/viewmodels/mywork3.js
+++ source/app/mywork/viewmodels/mywork3.js
@@ -1,22 +1,30 @@
 define(['durandal/app','knockout','plugins/router','plugins/dialog','mywork/mywork'], function (app,ko,router,dialog,mywork) {
    
-  
+      
        return  {
                 activate:function () {
-                          mywork.subtitle("mywork3");
-                          var navCls=$("#example-navbar-collapse").attr("class");
+                       
+                       
+                        var myrouter=mywork.getCurroute(this);
+                        console.log(myrouter);
+
+                        var navCls=$("#example-navbar-collapse").attr("class");
                           if (navCls=="navbar-collapse collapse in")
                                 {
                                         $(".navbar-toggle").trigger("click");
                                 }
+                         
                 },
-                 attached:function () {
-                       
-                        mywork.subtitle("mywork3");
+                attached:function () {
+                      
                        
                        
 
                 },
+                compositionComplete:function(){
+                       
+                }
+
               
         };
 }); 
