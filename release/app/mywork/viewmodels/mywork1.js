7fdcf9ac3fc4d5660bef9f08e4413bccde153fb4
fujia <esc@fujiadeMacBook-Air.local>
2016-11-22 17:56:32 +0800

    ggwp

--- release/app/mywork/viewmodels/mywork1.js
+++ release/app/mywork/viewmodels/mywork1.js
@@ -1,12 +1,11 @@
 define(['durandal/app','knockout','plugins/router','plugins/dialog','mywork/myworkbase','jquery','durandal/system'], function (app,ko,router,dialog,myworkbase,jquery,system) {
    // Use the settings object to change the theme
        var works1={
-               id:122,
-               clicktest:function (){alert("ddd");}
+                id:122,
+                clicktest:function (){
+                    
+                }
        };
-      
-        
         works1=jquery.extend(works1,myworkbase);
        return works1; 
-       
 }); 
