2bcf69efa7ef9384f7a48b2658e233e34effa666
fujia <5211666@qq.com>
2016-11-25 12:24:11 +0800

    优化

--- source/app/mywork/viewmodels/mywork1.js
+++ source/app/mywork/viewmodels/mywork1.js
@@ -8,6 +8,27 @@ define(['durandal/app','knockout','plugins/router','plugins/dialog','mywork/mywo
         works1=jquery.extend(works1,myworkbase);
        
         
+        works1._attached=function(){$(function() {
+                            var currYear = (new Date()).getFullYear();
+                            var opt = {};
+                            // opt.date = { preset: 'date' };
+                            opt.datetime = { preset: 'datetime' };
+                            // opt.time = { preset: 'time' };
+                            opt.default = {
+                                theme: 'sense-ui', //皮肤样式
+                                display: 'bottom', //显示方式
+                                mode: 'scroller', //日期选择模式
+                                dateFormat: 'yyyy-mm-dd',
+                                preset: 'datetime',
+                                lang: 'zh',
+                                showNow: true,
+                                nowText: "今天",
+                                startYear: currYear, //开始年份
+                                endYear: currYear + 2, //结束年份
+                            };
+                            $(".appDate").mobiscroll($.extend(opt['date'], opt['default']));
+                        });
+                    }
         
        
        return works1; 
